2ace33eb712196d4ef3e5f4adf200e9746b4a723
CtrAtlDelete <kvasovski34@gmail.com>
2022-09-30 16:36:17 +0300

    Add shadow embending

---
+++ constructor/constructorExmplEmbending.go
@@ -0,0 +1,20 @@
+package main
+
+import ("fmt")
+
+type SubBlock struct {
+	str string
+	integer int
+}
+
+type Block struct {
+	block SubBlock
+	str float32
+	integer float32
+}
+
+func main() {
+	var obj Block
+	fmt.Printf("%T\n", obj.block.integer)
+	fmt.Printf("%T\n", obj.integer)
+}
