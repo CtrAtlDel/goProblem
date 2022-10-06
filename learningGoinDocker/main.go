ffe2e2a91201aaa2dd562f41683a858af2a4598c
CtrAtlDelete <kvasovski34@gmail.com>
2022-10-06 16:43:00 +0300

    Add Go in Docker prototype

---
+++ learningGoinDocker/main.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt"
+"net/http")
+
+func main() {
+	http.HandleFunc("/docker", func(w http.ResponseWriter, req *http.Request){
+		fmt.Fprint(w, "<h1>Hello from docker!</h1>")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
