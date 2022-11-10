078d83253e972bae0e0812c5f5b021b0e9555956
CtrAtlDelete <kvasovski34@gmail.com>
2022-11-10 23:31:00 +0300

    Add new tests func in makefile

--- http-rest-api/internal/app/apiserver/apiserver_internal_test.go
+++ http-rest-api/internal/app/apiserver/apiserver_internal_test.go
@@ -1,2 +1,17 @@
 package apiserver
 
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApiServer_HandleHello(t *testing.T) {
+	s := New(NewConfig())                                    // создаем новый конфиг
+	rec := httptest.NewRecorder()                            // делаем новую запись
+	req, _ := http.NewRequest(http.MethodGet, "/hello", nil) // создаем новый http запрос
+	s.handleHello().ServeHTTP(rec, req) 
+	assert.Equal(t, rec.Body.String(), "hello")
+}
