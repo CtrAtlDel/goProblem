cf9fe73fc045d2eca1620c4722023e9bb34adb57
CtrAtlDelete <kvasovski34@gmail.com>
2022-11-13 14:21:38 +0300

    Add model test

--- http-rest-api/internal/app/model/user.go
+++ http-rest-api/internal/app/model/user.go
@@ -1,7 +1,32 @@
-package mode
+package model
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID                int
 	Email             string
+	Password          string
 	EncryptedPassword string
+}
+
+// BeforeCreate ...
+func (u *User) BeforeCreate() error {
+	if len(u.EncryptedPassword) > 0 {
+		enc, err := encryptString(u.Password)
+		if err != nil {
+			return nil
+		}
+		u.EncryptedPassword = enc
+	}
+	return nil
+}
+
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
